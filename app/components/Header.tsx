ce4b5be596e9cb45a61059c98b7b63675f3d94f9
Chirstian <tanedochrisjohn@gmail.com>
2025-02-26 13:52:27 +0800

    Remove Logout Error

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Menu, User, LogOut } from 'lucide-react';
+import { Menu, User } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
 import Link from 'next/link';
